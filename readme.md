1e78bdc5b5268e1e653048ff1a78ee7dfacded4f
andrew-craig <andrcra@gmail.com>
2025-05-20 23:45:56 +1000

    Update version of BirdNET-Analyzer

--- readme.md
+++ readme.md
@@ -11,7 +11,14 @@ Clone the BirdNet-Analyzer repository
 (this will take a while)
 
 Copy the `record-analyse.py` script into the BirdNET-Analyzer directory
-> scp /path/to/file 
+> scp /path/to/file
+
+Download the models
+> cd BirdNet-Analyzer/birdnet_analyzer
+> mkdir checkpoints
+> cd checkpoints
+> wget https://tuc.cloud/index.php/s/3BsizWy5M7CtQ5w/download/V2.4.zip
+
 
 Create a virtual environment
 > python -m venv venv
@@ -19,7 +26,7 @@ Create a virtual environment
 Activate the venv
 > source venv/bin/activate
 
-> pip install tflite-runtime librosa asyncio uuid7 pydub
+> pip install tflite-runtime librosa asyncio uuid7 pydub keras_tuner pyalsaaudio "numpy<2.0"
 
 Is librosa still needed?
 
@@ -34,13 +41,13 @@ Copy the below into the file. Check the username
     Description=Service to listen for birds via a mic
     StartLimitIntervalSec=300
     StartLimitBurst=5
-    
+
     [Service]
-    ExecStart=/home/operator/bird-listener/venv/bin/python /home/operator/bird-listener/BirdNET-Analyzer/recordAnalyse.py   
-    WorkingDirectory=/home/operator/bird-listener/BirdNET-Analyzer  
-    Restart=on-failure   
-    RestartSec=5s   
-    
+    ExecStart=/home/operator/bird-listener/venv/bin/python /home/operator/bird-listener/BirdNET-Analyzer/recordAnalyse.py
+    WorkingDirectory=/home/operator/bird-listener/BirdNET-Analyzer
+    Restart=on-failure
+    RestartSec=5s
+
     [Install]
     WantedBy=multi-user.target
 
--- recordAnalyse.py
+++ recordAnalyse.py
@@ -5,10 +5,10 @@ import alsaaudio
 import numpy as np
 import math
 import operator
-import analyze
-import utils
-import config as cfg
-import species
+from birdnet_analyzer.analyze.utils import load_codes, predict
+import birdnet_analyzer.utils as utils
+import birdnet_analyzer.config as cfg
+from birdnet_analyzer.species.utils import get_species_list
 import sqlite3
 import yaml
 from zoneinfo import ZoneInfo
@@ -17,8 +17,8 @@ from pathlib import Path
 import pydub
 
 def confirm_dir(dir: Path):
-    if not dir.is_dir(): 
-        os.mkdir(dir) 
+    if not dir.is_dir():
+        os.mkdir(dir)
     return True
 
 def startup(w_dir):
@@ -30,9 +30,8 @@ def startup(w_dir):
 
     script_dir = w_dir / 'BirdNET-Analyzer'
 
-
     cfg.MODEL_PATH = os.path.join(script_dir, cfg.MODEL_PATH)
-    cfg.LABELS_FILE = os.path.join(script_dir, cfg.LABELS_FILE)
+    cfg.LABELS_FILE = os.path.join(script_dir, 'birdnet_analyzer/labels/V2.4/BirdNET_GLOBAL_6K_V2.4_Labels_en_uk.txt')
     cfg.TRANSLATED_LABELS_PATH = os.path.join(script_dir, cfg.TRANSLATED_LABELS_PATH)
     cfg.MDATA_MODEL_PATH = os.path.join(script_dir, cfg.MDATA_MODEL_PATH)
     cfg.CODES_FILE = os.path.join(script_dir, cfg.CODES_FILE)
@@ -42,17 +41,17 @@ def startup(w_dir):
     cfg.SIGMOID_SENSITIVITY = 1.0
     cfg.CPU_THREADS = 1
     cfg.SAMPLE_RATE = 48000
-    cfg.CODES = analyze.loadCodes()
-    cfg.LABELS = utils.readLines(cfg.LABELS_FILE)
+    cfg.CODES = load_codes()
+    cfg.LABELS = utils.read_lines(cfg.LABELS_FILE)
     cfg.TRANSLATED_LABELS = cfg.LABELS
     cfg.LOCATION_FILTER_THRESHOLD = 0.03
     cfg.WEEK = datetime.date.today().isocalendar()[1] # could change this to read from the recording file name
-    
+
 
     conf = yaml.safe_load(open('config.yaml'))
     cfg.LATITUDE = conf['latitude']
     cfg.LONGITUDE = conf['longitude']
-    cfg.SPECIES_LIST = species.getSpeciesList(cfg.LATITUDE, cfg.LONGITUDE, cfg.WEEK, cfg.LOCATION_FILTER_THRESHOLD)
+    cfg.SPECIES_LIST = get_species_list(cfg.LATITUDE, cfg.LONGITUDE, cfg.WEEK, cfg.LOCATION_FILTER_THRESHOLD)
 
 
     # add database configuraiton
@@ -73,7 +72,7 @@ def save_audio(species_name, signal, start_ts, w_dir):
     a = pydub.AudioSegment(signal.tobytes(), frame_rate=cfg.SAMPLE_RATE, sample_width=2, channels=1)
     n = 'ts_' + str(start_ts) + '.flac'
     d = w_dir / 'recordings' / species_name
-    confirm_dir(d) 
+    confirm_dir(d)
     fn = d / n
     a.export(fn, format="flac")
     return True
@@ -84,7 +83,7 @@ def analyse_recording(start_ts, signal):
     scaled_sig = signal / 32768
 
     # Run the analysis
-    pred = analyze.predict([scaled_sig])[0]
+    pred = predict([scaled_sig])[0]
 
     # Assign scores to labels
     p_labels = zip(cfg.LABELS, pred)
@@ -131,8 +130,7 @@ def analyse_recording(start_ts, signal):
         else:
             pass
 
-        return filtered_detections       
-
+        return filtered_detections
 
 async def recording_worker(queue, input, num_periods=140):
     while True:
@@ -142,7 +140,7 @@ async def recording_worker(queue, input, num_periods=140):
             l, data = input.read()
             frames.append(data)
             await asyncio.sleep(0.01)
-        
+
         print("Completed recording loop")
         await queue.put((start_ts, frames))
 
@@ -160,7 +158,7 @@ async def analysis_worker(queue, max_queue_size, w_dir):
 
             # save the file
             # EXCLUSION_LIST = ['Trichoglossus moluccanus_Rainbow Lorikeet','Cacatua galerita_Sulphur-crested Cockatoo','Strepera graculina_Pied Currawong','Alisterus scapularis_Australian King-Parrot','Fulica atra_Eurasian Cootz','Gymnorhina tibicen_Australian Magpie']
-            # if len(detections) == 1 and detections[0] not in EXCLUSION_LIST: 
+            # if len(detections) == 1 and detections[0] not in EXCLUSION_LIST:
             #     save_audio(detections[0], signal, start_ts, w_dir)
 
             print('Analysed signal')
@@ -168,7 +166,6 @@ async def analysis_worker(queue, max_queue_size, w_dir):
         await asyncio.sleep(0.5)
         # Notify the queue that the "work item" has been processed.
         queue.task_done()
-
 
 async def main():
     w_dir = Path.cwd().parent
@@ -178,7 +175,7 @@ async def main():
     record_seconds = 3
     period_size = 1024
     queue = asyncio.Queue()
-    
+
     input = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NORMAL,
 		channels=1, rate=rate, format=alsaaudio.PCM_FORMAT_S16_LE,
 		periodsize=period_size, device='default')
